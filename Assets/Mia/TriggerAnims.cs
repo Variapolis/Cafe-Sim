82a9c8c2ba19b6c7524613bd618845f40d999e7b
Vari <72755263+Variapolis@users.noreply.github.com>
2022-10-31 15:16:06 +0000

    feat(mia): implement dynamic animation ui

--- Assets/Mia/TriggerAnims.cs
+++ Assets/Mia/TriggerAnims.cs
@@ -1,52 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TriggerAnims : MonoBehaviour
 {
-    // Start is called before the first frame update
-    Animator anim;
-    public string[] aTriggers = {"Talk A", "Talk B", "Talk C"};
-    private KeyCode[] keyCodes = {
-         KeyCode.Alpha1,
-         KeyCode.Alpha2,
-         KeyCode.Alpha3
-     };
+    [SerializeField] private Animator anim;
+    [SerializeField] private ScrollRect scrollRect;
+    [SerializeField] private Button buttonPrefab;
 
     void Start()
     {
-        anim = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if (Input.GetKeyUp("4"))
-        //{
-        //    anim.SetTrigger("Idle");
-        //}
-        //if (Input.GetKeyUp("5"))
-        //{
-        //    anim.SetTrigger("IdleArmsOut");
-        //}
-        //if (Input.GetKeyUp("6"))
-        //{
-        //    anim.SetTrigger("IdleOffensive");
-        //}
-        // Below Code https://answers.unity.com/questions/420324/get-numeric-key.html
-        //if (Input.anyKey)
-        //{
-        for (int i = 0; i < keyCodes.Length; i++)
-            {
-                if (Input.GetKeyDown(keyCodes[i]))
-                {
-                    int numberPressed = i+1;
-                    Debug.Log(numberPressed);
-                    string animtoPlay = aTriggers[numberPressed-1];
-                    anim.SetTrigger(animtoPlay);
-                }
-            }
-        //}
+        foreach (var param in anim.parameters)
+        {
+            var button = Instantiate(buttonPrefab, scrollRect.content);
+            button.GetComponentInChildren<TMP_Text>().text = $"Anim {param.name}";
+            button.onClick.AddListener(() => anim.SetTrigger(param.name));
+        }
     }
 }
