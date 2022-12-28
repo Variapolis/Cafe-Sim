914653d79fff636dbdf50dc6d6a870ae6ced4757
Vari <tmarksinbox@gmail.com>
2022-12-28 02:02:35 +0000

    feat(customer): add customer character prefab with custom animation controller

---
+++ Assets/CustomerAI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CustomerAI : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Transform waypoint;
+
+    private void Start() => navMeshAgent.destination = waypoint.position;
+
+    void Update() => animator.SetFloat("WalkSpeed", navMeshAgent.velocity.magnitude);
+}
