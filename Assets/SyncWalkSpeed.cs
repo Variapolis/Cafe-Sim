c88096c0a7ce125b949472450bb1185763b525c0
Vari <tmarksinbox@gmail.com>
2023-01-05 06:34:34 +0000

    refactor(customer): strip ai behaviour from SyncWalkSpeed.cs renamed from CustomerAI.cs

--- Assets/SyncWalkSpeed.cs
+++ Assets/SyncWalkSpeed.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
-public class CustomerAI : MonoBehaviour
+public class SyncWalkSpeed : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent navMeshAgent;
     [SerializeField] private Animator animator;
-    [SerializeField] private Transform waypoint;
-
-    private void Start() => navMeshAgent.destination = waypoint.position;
-
+    
     void Update() => animator.SetFloat("WalkSpeed", navMeshAgent.velocity.magnitude);
 }
