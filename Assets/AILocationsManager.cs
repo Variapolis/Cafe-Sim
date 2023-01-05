f149b8b385636c6e80eae6976403dbb07ca7501f
Vari <tmarksinbox@gmail.com>
2023-01-05 06:35:31 +0000

    feat(customer): add AILocationsManager.cs and Restaurant.cs

---
+++ Assets/AILocationsManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AILocationsManager : MonoBehaviour
+{
+    public static AILocationsManager Instance;
+    public List<Restaurant> Restaurants = new();
+    public Transform[] PointsOfInterest;
+
+    private void Awake()
+    {
+        if (Instance) Destroy(this);
+        else Instance = this;
+    }
+}
---
+++ Assets/Restaurant.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Cafe.CookingSystem;
+using UnityEngine;
+
+public class Restaurant : MonoBehaviour
+{
+    [SerializeField] private FoodMenu foodMenu;
+    public bool IsOpen => OpenKiosks.Count > 0;
+    public List<Kiosk> OpenKiosks = new();
+    public List<Kiosk> ClosedKiosks = new();
+    public FoodMenu FoodMenu => foodMenu;
+    private void Start() => AILocationsManager.Instance.Restaurants.Add(this);
+}
