e9737dc64be16c4bb7e4dc8196b2546531b40be9
Vari <tmarksinbox@gmail.com>
2023-01-05 06:35:49 +0000

    refactor(food): add more info to FoodMenuItem.cs

--- Assets/Cafe/CookingSystem/FoodMenuItem.cs
+++ Assets/Cafe/CookingSystem/FoodMenuItem.cs
@@ -5,9 +5,11 @@ namespace Cafe.CookingSystem
     [CreateAssetMenu]
     public sealed class FoodMenuItem : ScriptableObject
     {
-        [SerializeField] private string _name;
-        [SerializeField] private Color _nameColor;
-        public string Name => $"<color=#{ColorUtility.ToHtmlStringRGB(_nameColor)}>{_name}</color>";
+        [SerializeField] private string name;
+        [SerializeField] private Color color;
+        public string Name => $"<color=#{ColorUtility.ToHtmlStringRGB(color)}>{name}</color>";
+        public string PlainTextName => name;
+        public Color Color => color;
         public Ingredient[] Ingredients;
     }
 }
