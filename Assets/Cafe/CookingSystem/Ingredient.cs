82cf00b4a90e4b691274a2ff4439b29bd8e797ce
Vari <tmarksinbox@gmail.com>
2023-01-05 06:36:49 +0000

    refactor(food): lock Ingredient.cs vertically on pickup and placement

--- Assets/Cafe/CookingSystem/Ingredient.cs
+++ Assets/Cafe/CookingSystem/Ingredient.cs
@@ -44,6 +44,7 @@ namespace Cafe.CookingSystem
             {
                 transform.parent = holder;
                 transform.position = holder.transform.position;
+                transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
                 _rigidbody.detectCollisions = false;
                 _rigidbody.isKinematic = true;
             }
@@ -60,6 +61,7 @@ namespace Cafe.CookingSystem
         public void Drop(Vector3 position)
         {
             transform.parent = null;
+            transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
             _rigidbody.isKinematic = false;
             _rigidbody.detectCollisions = true;
             transform.position = position;
