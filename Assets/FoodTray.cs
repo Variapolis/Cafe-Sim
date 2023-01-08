a12748336560fb3e81c025e1feeb050b543dd06a
Vari <tmarksinbox@gmail.com>
2023-01-08 16:12:40 +0000

    fix(tray): fix tray attachment points

--- Assets/FoodTray.cs
+++ Assets/FoodTray.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class FoodTray : MonoBehaviour, IItemInteractable
 {
     public List<FoodItem> FoodItems;
+    [SerializeField] private Transform[] attachmentPoints;
+    private int nextAttachmentPoint;
 
     public bool InteractWithItem(GameObject item)
     {
+        if (nextAttachmentPoint >= attachmentPoints.Length) return false;
         if (!item.TryGetComponent<FoodItem>(out var ingredient)) return false;
         var rb = item.GetComponent<Rigidbody>();
-        item.transform.parent = transform;
-        item.transform.position = transform.position;
+        var attachmentPoint = attachmentPoints[nextAttachmentPoint];
+        item.transform.parent = attachmentPoint;
+        nextAttachmentPoint++;
+        item.transform.position = attachmentPoint.position;
+        item.transform.rotation = Quaternion.identity;
         rb.isKinematic = true;
         rb.detectCollisions = false;
         FoodItems.Add(ingredient);
