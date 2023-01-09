cedcb525232fbc3e533cabe3c4684f335db28ec2
Vari <tmarksinbox@gmail.com>
2023-01-09 01:50:49 +0000

    build: add menu and game over scenes

--- Assets/ScoreUI.cs
+++ Assets/ScoreUI.cs
@@ -1,12 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private TMP_Text timerText;
     [SerializeField] private Restaurant restaurant;
-    private const float time = 600;
+    private const int scoreNeeded = 10;
+    private const float time = 25;
     private float startTime;
     void Start() => startTime = Time.time;
 
@@ -14,7 +16,9 @@ public class ScoreUI : MonoBehaviour
     void Update()
     {
         var timeSince = time - (Time.time - startTime);
+        Debug.Log(timeSince);
         timerText.text = $"Time Left: {Mathf.Floor(timeSince / 60)}:{Mathf.Floor(timeSince % 60).ToString("00")}";
         scoreText.text = $"Score: {restaurant.score}";
+        if (timeSince <= 0) SceneManager.LoadScene(restaurant.score < scoreNeeded ? "FailedScene" : "SuccessScene");
     }
 }
--- Assets/StopCoilWhine.cs
+++ Assets/StopCoilWhine.cs
@@ -1,13 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StopCoilWhine : MonoBehaviour
 {
+    [SerializeField] private bool CursorFree;
+
     // Start is called before the first frame update
     void Start()
     {
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
+        Cursor.lockState = CursorFree ? CursorLockMode.Confined : CursorLockMode.Locked;
+        Cursor.visible = CursorFree;
     }
 }
